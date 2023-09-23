fbf2c15b5a8c60ef5b411ad7cecb78caa3f401ef
iv_al3x <tripanosomas@ya.ru>
2023-09-23 10:20:39 +0300

    removed spring security and added some vaadin stuff

--- src/main/java/com/example/myfavouritemoney/MyFavouriteMoneyApplication.java
+++ src/main/java/com/example/myfavouritemoney/MyFavouriteMoneyApplication.java
@@ -2,17 +2,11 @@ package com.example.myfavouritemoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.Map;
 
 @SpringBootApplication
-@RestController
 public class MyFavouriteMoneyApplication {
 
 	//https://spring.io/guides/tutorials/spring-boot-oauth2/ - основная статья
@@ -20,10 +14,6 @@ public class MyFavouriteMoneyApplication {
 
 	//https://www.baeldung.com/spring-security-5-oauth2-login - статья в баелдунге
 	//Надо будет по итогам переделать security и избавиться от устаревших классов
-	@GetMapping("/user")
-	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		return Collections.singletonMap("name", principal.getAttribute("name"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFavouriteMoneyApplication.class, args);
